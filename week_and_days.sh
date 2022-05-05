f1d9bf09a938022529d9b554e25f9203259fec77
Geetanjali Koli <k0ligeet@hotmail.com>
2022-05-05 14:34:42 +0530

    Added: Program 10

---
+++ week_and_days.sh
@@ -0,0 +1,27 @@
+# Read Number and display the week day (Sunday to Saturday)
+
+echo "Program Execution to display any of the week day.."
+
+read -p "Enter the number: " week_num
+
+echo "Week day " $week_num "is : "
+
+case $week_num in
+        1) echo "Sunday"
+                ;;
+        2) echo "Monday"
+                ;;
+        3) echo "Tuesday"
+                ;;
+        4) echo "Wednesday"
+                ;;
+        5) echo "Thursday"
+                ;;
+        6) echo "Friday"
+                ;;
+        7) echo "Saturday"
+                ;;
+        *) echo "Spelling Mistake no such value present!!!"
+esac
+
+
